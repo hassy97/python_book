cd444f1bbfd0169072ae5b9c64fb87d3757ae3a0
Hassy <97879833+hassy97@users.noreply.github.com>
2022-01-31 19:27:12 +0500

    Add files via upload

---
+++ extras/NLP/text_represent.py
@@ -0,0 +1,29 @@
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer 
+
+''' how to strcture the textful data into the 
+    rows colum for the future machine learning
+    models predictions'''
+
+# corpus 
+corpus =['my name is hassy']
+
+# make an object for the CountVectorizer
+vec = CountVectorizer()
+# for structuring the text data we use fit_tranform ()
+
+Structure_data = vec.fit_transform(corpus)
+print()
+# print(Structure_data)
+
+# convert the text data  into the frequency based representation for future use in ml predictions
+print(Structure_data.toarray())
+
+# get the features unique words  from the corpus 
+vec.get_feature_names()
+
+vec.vocabulary_
+
+#print(vec.vocabulary_)
+
+print(len(vec.get_feature_names()))
