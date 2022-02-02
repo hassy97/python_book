035c76e0c9d3bc79fe8a271f88799949e17474c9
hassy97 <muhammad.hassaan97.skipq@gmail.com>
2022-02-02 17:17:42 +0500

    sucks

--- code/finger_2.4.py
+++ code/finger_2.4.py
@@ -5,19 +5,19 @@ should print a message to that effect.'''
 # these are some restriction use only if-else,while,variable assignment,operators and typecast
 ans = None
 y = 10
-itersLeft = y
+itersLeft = y 
 
 
 while(itersLeft > 0):
     user_input = int(input('Type a integer number'))
 
-    if  itersLeft == 10  or (user_input % 2 and user_input > ans):   # largest odd case 
+    if  itersLeft == 10  or (user_input % 2 and user_input > ans):   
         ans = user_input
     if user_input % 2 == 0:   # even case
         ans = user_input
     itersLeft = itersLeft - 1
 
-if ans%2 !=0:
+if ans%2 ==1: ## largest odd case 
     print("The largets odd number is : ", str(ans))
 else:
     print("no odd number was entered")
